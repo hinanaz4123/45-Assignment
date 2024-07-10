161c3158237dd8c142b3b9c962be656a1b518a17
hinanaz <hinanaz4123@gmail.com>
2024-07-10 21:16:24 +0500

    first commit

---
+++ question 2/main .js
@@ -0,0 +1,3 @@
+let personName = 'Taha';
+console.log(`Hello ${personName}, would you like to learn some python today?`);
+export {};
---
+++ question 2/main .ts
@@ -0,0 +1,3 @@
+let personName :string = 'Taha';
+
+console.log(`Hello ${personName}, would you like to learn some python today?`)
