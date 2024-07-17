97d4a6afa8d99b4201348914ac256892ad655b0c
hinanaz <hinanaz4123@gmail.com>
2024-07-17 20:42:03 +0500

    q5 commit

---
+++ question 5/main.ts
@@ -0,0 +1,5 @@
+let famous_person :string ='Albert Einsteina';
+
+let message :string = '"A person who never made a mistake never tried anything new."';
+
+console.log(`${famous_person} once said, ${message}`);
